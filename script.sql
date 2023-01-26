2d11d1d9c96369752eaf14a1f001b75fe6d0f607
KillianLynch <killian.lynch@oracle.com>
2023-01-26 13:31:50 -0600

    update the script

--- script.sql
+++ script.sql
@@ -1,7 +1,17 @@
 
-@/home/oracle/Downloads/MovieStream_DDL.sql
-@/home/oracle/Downloads/genre.sql
-@/home/oracle/Downloads/cust_segment.sql
-@/home/oracle/Downloads/cust.sql
-@/home/oracle/Downloads/movie.sql
-@/home/oracle/Downloads/custsales.sql
+CONNECT / AS SYSDBA;
+
+CREATE USER 19lab IDENTIFIED BY 19lab;
+
+GRANT DBA TO 19lab;
+
+ALTER SESSION SET CONTAINER=PDB1;
+
+CONNECT 19lab/19lab;
+
+@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
+@/home/oracle/kl_moviestream-main/genre.sql
+@/home/oracle/kl_moviestream-main/cust_segment.sql
+@/home/oracle/kl_moviestream-main/cust.sql
+@/home/oracle/kl_moviestream-main/movie.sql
+@/home/oracle/kl_moviestream-main/custsales.sql
