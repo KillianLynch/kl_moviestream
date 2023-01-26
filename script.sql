a2b6576a6e03cf640a276cac05f6c256fe4564f6
KillianLynch <killian.lynch@oracle.com>
2023-01-26 14:12:49 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -7,9 +7,9 @@ ALTER SESSION SET CONTAINER=PDB1;
 
 CONNECT 19lab/19lab;
 
-@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
-@/home/oracle/kl_moviestream-main/genre.sql
-@/home/oracle/kl_moviestream-main/cust_segment.sql
-@/home/oracle/kl_moviestream-main/cust.sql
-@/home/oracle/kl_moviestream-main/movie.sql
-@/home/oracle/kl_moviestream-main/custsales.sql
+--@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
+--@/home/oracle/kl_moviestream-main/genre.sql
+--@/home/oracle/kl_moviestream-main/cust_segment.sql
+--@/home/oracle/kl_moviestream-main/cust.sql
+--@/home/oracle/kl_moviestream-main/movie.sql
+--@/home/oracle/kl_moviestream-main/custsales.sql
