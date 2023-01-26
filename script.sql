3f0c5749b097e529420e415dec905118359ced97
KillianLynch <killian.lynch@oracle.com>
2023-01-26 14:38:57 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
 
 ALTER SESSION SET CONTAINER=PDB1;
 
-CREATE USER nf19c IDENTIFIED BY nf19c;
+CREATE USER nf19c IDENTIFIED BY WElcome123##;
 
 GRANT DBA TO nf19c;
 
-CONNECT nf19c/nf19c;
+CONNECT nf19c/WElcome123##;
 
 --@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
 --@/home/oracle/kl_moviestream-main/genre.sql
