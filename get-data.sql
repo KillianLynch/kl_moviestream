1a2a3fd53fe78f8c10968831bf8e502a19965d69
KillianLynch <killian.lynch@oracle.com>
2023-02-01 15:43:31 -0600

    updates

---
+++ get-data.sql
@@ -0,0 +1,7 @@
+
+@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
+@/home/oracle/kl_moviestream-main/genre.sql
+@/home/oracle/kl_moviestream-main/cust_segment.sql
+@/home/oracle/kl_moviestream-main/cust.sql
+@/home/oracle/kl_moviestream-main/movie.sql
+@/home/oracle/kl_moviestream-main/custsales.sql
--- script.sql
+++ script.sql
@@ -3,13 +3,5 @@ ALTER SESSION SET CONTAINER=PDB1;
 
 CREATE USER nf19c IDENTIFIED BY WElcome123##;
 
+GRANT CONNECT TO nf19c;
 GRANT DBA TO nf19c;
-
---CONNECT nf19c/WElcome123##;
-
---@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
---@/home/oracle/kl_moviestream-main/genre.sql
---@/home/oracle/kl_moviestream-main/cust_segment.sql
---@/home/oracle/kl_moviestream-main/cust.sql
---@/home/oracle/kl_moviestream-main/movie.sql
---@/home/oracle/kl_moviestream-main/custsales.sql
