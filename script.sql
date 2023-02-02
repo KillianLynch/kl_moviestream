f14f0f388436f4471161ddc481ff702a0a38d3bb
KillianLynch <killian.lynch@oracle.com>
2023-02-02 13:01:26 -0600

    updates to script for pdb connection

--- script.sql
+++ script.sql
@@ -5,3 +5,12 @@ CREATE USER nf19c IDENTIFIED BY WElcome123##;
 
 GRANT CONNECT TO nf19c;
 GRANT DBA TO nf19c;
+
+connect nf19c/WElcome123##@//ll19c.pub.db19cnf.oraclevcn.com:1521/pdb1.pub.db19cnf.oraclevcn.com
+
+@/home/oracle/kl_moviestream-main/MovieStream_DDL.sql
+@/home/oracle/kl_moviestream-main/genre.sql
+@/home/oracle/kl_moviestream-main/cust_segment.sql
+@/home/oracle/kl_moviestream-main/cust.sql
+@/home/oracle/kl_moviestream-main/movie.sql
+@/home/oracle/kl_moviestream-main/custsales.sql
